0595625bb5be5348004d31f0df22024260353249
rifat71 <rifatmahmud24311@gmail.com>
2023-04-25 14:38:49 +0600

    add cucumber.js file

---
+++ cucumber.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'default': 'features/ --require features/ --publish-quiet',
+    dry: '--dry-run',
+    summary: '--format summary',
+    progress: '--format progress',
+    html_report: '--format json:reports/cucumber_report.json',
+    parallel: '--parallel 2'
+}
